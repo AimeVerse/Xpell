1acb2157874d719a6c3b30079843b0f20cbd822a
Captain Crypto <tamir@crypto-knight.tech>
2022-07-28 19:59:27 +0300

    xchanges

---
+++ xpell.ts
@@ -0,0 +1,16 @@
+export {default as XPell} from "./src/XPell"
+export {XUtils} from "./src/XUtils"
+export {XData} from "./src/XData"
+export {XParser} from "./src/XParser"
+export {XCommand} from "./src/XCommand"
+export {XLogger} from "./src/XLogger"
+export {XModule} from "./src/XModule"
+export {XObject,XObjectPack} from "./src/XObject"
+export {XObjectManager} from "./src/XObjectManager"
+export {XEventManager,XEvent,XEventList} from "./src/XEventManager"
+export {XUI} from "./src/XUI/XUI"
+export {XUIObject} from "./src/XUI/XUIObject"
+export {XUIObjects,XView,XButton,XForm,XHeader,XImage,XLabel,XLink,XList,XNavBar,XTextArea,XTextField,XVideo,XWebcam} from "./src/XUI/XUICoreObjects"
+export {XViewManager} from "./src/XUI/XViewManager"
+
+
