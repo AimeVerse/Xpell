cd14b96e814431ac426ddfbd4a6a7889c88b6a49
Captain Crypto <tamir@crypto-knight.tech>
2022-08-09 00:55:11 +0300

    docs

--- src/XLogger.ts
+++ src/XLogger.ts
@@ -5,16 +5,31 @@
 
 
 
-
+/**
+ * @class XLoggerEngine Xpell Logger engine
+ */
 export class XLoggerEngine {
+    /**
+     * Enable logger activity if false no logs will be displayed
+     */
     enabled: boolean = true
+    /**
+     * Show the date in every log message
+     */
     showDate:boolean = false
+    /**
+     * Show the Time in every log message
+     */
     showTime:boolean = true
 
     constructor() {
     }
 
-    private getTimeSignature() {
+    /**
+     * Generates the log output date/time signature (affected by showDate & showTime properties)
+     * @returns {string}
+     */
+    private getLogDateTimeSignature():string {
         const d = new Date()
 
         const getDate = () => {return (this.showDate) ? d.getDate() + '.' + d.getMonth() + '.' + d.getFullYear() + " ": "" }
@@ -22,17 +37,27 @@ export class XLoggerEngine {
         return  getDate() + getTime()
     }
 
+    /**
+     * Log a message to the output log (console)
+     * @param message - message to present
+     * @param optionalParams 
+     */
     log(message?: any, ...optionalParams: any[]) {
         if (this.enabled) {
             var args = Array.prototype.slice.call(arguments);
-            args.unshift(this.getTimeSignature());
+            args.unshift(this.getLogDateTimeSignature());
             console.log.apply(console, args);
         }
     }
 
+    /**
+     * Log an error message to the output log (console)
+     * @param message - message to present
+     * @param optionalParams 
+     */
     error(message?: any, ...optionalParams: any[]) {
         var args = Array.prototype.slice.call(arguments);
-        args.unshift(this.getTimeSignature());
+        args.unshift(this.getLogDateTimeSignature());
         console.error.apply(console, args);
 
     }
