af23b43edba1457dade2175885a3b7810607a303
Captain Crypto <tamir@crypto-knight.tech>
2024-01-21 14:34:31 +0200

    index ts

--- index.ts
+++ index.ts
@@ -4,24 +4,6 @@
  * Library Entry Point
  * 
  * @description Universal User Interface (UI) Engine for Javascript supporting devices & browsers
- * @author Fridman Fridman <tamirf@yahoo.com>
- * @since  22/07/2022
- * @Copyright Fridman Tamir 2022, all right reserved
- *
- *      This program is free software; you can redistribute it and/or
- *		modify it under the terms of the GNU General Public License
- *		as published by the Free Software Foundation; either version
- *		3 of the License, or (at your option) any later version.
- *
- */
-
-
-/**
- * Xpell - Real-Time User Interface Platform
- * Typescript Edition
- * Library Entry Point
- * 
- * @description Universal User Interface (UI) Engine for Javascript supporting devices & browsers
  * @author Fridman Fridman <fridman.tamir@gmail.com>
  * @since  22/07/2022
  * @Copyright Fridman Tamir 2022, all right reserved
@@ -52,6 +34,9 @@ export {
     XEventManager,XEventManager as _xem,_XEventManager,
     type HTMLEventListenersIndex,type XEventListenerOptions
 } from "./src/Core/Xpell"
+
+export {Wormholes,Wormholes as _wh,WormholeEvents,type MessageType} from "./src/Wormholes/Wormholes"
+
 
 export {XUI,type XUIApp,XUIModule} from "./src/XUI/XUI"
 
